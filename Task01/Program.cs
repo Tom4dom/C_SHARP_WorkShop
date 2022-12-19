00e97c6c8089cd774121e4ad9686a39e89c39e77
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-19 14:20:16 +0300

    Task01_my_own_version_of_this_task

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input integer 'A': ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input integer 'B'");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int numberC = numberA/numberB;
+
+if (numberB == numberC)
+{
+    Console.WriteLine($"Yes! It is true that {numberA} is a square of {numberB}");
+}
+else
+{
+    Console.WriteLine($"No! It is not true that {numberA} is square of {numberB}");
+}
